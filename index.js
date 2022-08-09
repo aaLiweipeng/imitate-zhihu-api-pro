2b4b5d6fa9317d67a6ce07acf3eb515523bdb099
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-08-09 23:32:57 +0800

    洋葱模型

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use((ctx) => {
+// next用于执行 下一个中间件
+app.use(async (ctx, next) => {
+    console.log(11111111);
+    await next(); 
+    console.log(22222222);
     ctx.body = 'Hello World!!!';
+});
+
+app.use(async (ctx, next) => {
+  console.log(333333333);
+  await next();
+  console.log(444444444);
+});
+
+app.use(async (ctx, next) => {
+  console.log(55555555);
 });
 
 app.listen(3000);
