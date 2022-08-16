99534446a09b00f905140e6e287d892e68a9e9ba
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-08-17 00:14:40 +0800

    koa自带错误处理 实践

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,13 +2,20 @@ const dbStorage = [{ name: "李雷" }]; // 内存模拟数据库
 
 class UsersCtl {
   find(ctx) {
+    // 模拟一个运行时错误【undefined.undefined】(语法没问题，运行时错误)
+    // a.b
+
     // 直接返回 整个数据对象
     ctx.body = dbStorage;
   }
-  
+
   findById(ctx) {
     // ctx.params.id * 1  是为了把 String类型 转换成 Number类型
     // 以id为索引取数据
+    if (ctx.params.id * 1 > dbStorage.length - 1) {
+      // ctx.throw(412);
+      ctx.throw(412, '先决条件失败：id参数 大于数组长度-1，索引越界了！');
+    }
     ctx.body = dbStorage[ctx.params.id * 1];
   }
 
