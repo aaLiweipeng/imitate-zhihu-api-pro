63af8ada85e84e458a44f851b41918e293ad01fb
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-08-19 03:17:36 +0800

    安装koa-json-error、cross-env，配置环境脚本，处理错误请求

--- app/index.js
+++ app/index.js
@@ -6,8 +6,20 @@ const app = new Koa();
 const routing = require('./routes');
 
 // 错误处理模块的引入、注册
-const { test500 } = require("./controllers/error");
-app.use(test500);
+// const { test500 } = require("./controllers/error");
+// app.use(test500);
+
+// 引入koa-json-error
+const error = require('koa-json-error');
+// app.use(error());// 默认配置注册
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) => 
+      // 如果是生产环境，返回除了堆栈的其他内容；
+      // 其他环境，堆栈和其他内容 都一起返回
+      process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
+  })
+);
 
 app.use(bodyParser());
 routing(app);
