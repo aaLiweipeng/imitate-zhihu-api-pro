ab2d10eb5b46a40dc8d989f677120a2362faccdb
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2022-08-21 19:23:46 +0800

    引入mongoose，连接数据库

---
+++ app/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  connectionStr:
+    "mongodb+srv://liweipeng:123456789aaa@mzhihu.sfy8sih.mongodb.net/?retryWrites=true&w=majority",
+};
--- app/index.js
+++ app/index.js
@@ -2,17 +2,23 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const app = new Koa();
 
-// routes整体注册模块
-const routing = require('./routes');
+const routing = require('./routes'); // routes整体注册模块
 
 // 错误处理模块的引入、注册
 // const { test500 } = require("./controllers/error");
 // app.use(test500);
 
-// 引入koa-json-error
-const error = require('koa-json-error');
-// 引入koa-parameter
-const parameter = require('koa-parameter');
+const error = require('koa-json-error'); // 引入koa-json-error
+const parameter = require('koa-parameter');// 引入koa-parameter
+
+const mongoose = require('mongoose');// 引入mongoose
+const { connectionStr } = require("./config");
+mongoose.connect(connectionStr, () => console.log("MongoDB 连接成功!!!"));
+// mongoose.connection.on('error', console.error);
+mongoose.connection.on("error", () => {
+  console.error;
+  // console.log("MongoDB 连接失败!!!");
+});
 
 // app.use(error());// 默认配置注册
 app.use(
